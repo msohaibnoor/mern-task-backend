5fad3d6af73a74b74720342185f705d2a0b219d2
Sohaib <sohaibfarrukh59@gmail.com>
2024-05-11 19:49:14 +0500

    project set up and register route

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const app = require('./app');
+// const config = require('./config/config');
+// const logger = require('./config/logger');
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+let server;
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log('Connected to MongoDB');
+  server = app.listen(process.env.PORT, () => {
+    console.log(`Listening to port ${process.env.port}`);
+  });
+}); 
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      console.log('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  console.log(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
---
+++ src/models/token.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const tokenSchema = mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+/**
+ * @typedef Token
+ */
+const Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
